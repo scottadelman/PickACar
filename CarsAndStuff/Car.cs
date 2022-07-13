7fa395d560c2031486fcf25bf213b290994dc2be
Scott <ScottAdelman97@gmail.com>
2022-07-13 14:14:42 -0600

    Pick a car!

---
+++ CarsAndStuff/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsAndStuff;
+
+internal class Car
+{
+    public Car()
+    {
+
+    }
+
+    public Car(string make, string model, int year, string trim)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        Trim = trim;
+    }   
+
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Trim { get; set; }
+
+
+    public void PrintCarInfo()
+    {
+        Console.WriteLine($"Your Car is a {Year} {Make} {Model} {Trim}");
+    }
+}
+
---
+++ CarsAndStuff/Program.cs
@@ -0,0 +1,28 @@
+﻿using CarsAndStuff;
+
+var car1 = new Car();
+car1.Make = "Toyota";
+car1.Model = "4Runner";
+car1.Year = 2020;
+car1.Trim = "TRD Offroad-Premium";
+
+var car2 = new Car();
+car2.Make = "Hummer";
+car2.Model = "H1";
+car2.Year = 2006;
+
+Console.WriteLine("Would you like a hummer or a 4runner?");
+var answer = Console.ReadLine();
+
+if (answer == "4runner")
+{
+    car1.PrintCarInfo();
+}
+else if (answer == "hummer")
+    {
+        car2.PrintCarInfo();
+    }
+else 
+    {
+        Console.WriteLine("I dont have that car, leave my dealership now!");
+    }
